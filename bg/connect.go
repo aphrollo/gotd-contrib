68df2f230b82c847ffb7edeab5f6ee9c86369440
sprayKING <trickstertwo@gmail.com>
2025-08-25 00:12:07 +0200

    background run - implemented floodwait middleware

--- bg/connect.go
+++ bg/connect.go
@@ -1,21 +1,24 @@
-// Package bg implements wrapper for running client in background.
 package bg
 
 import (
 	"context"
 	"errors"
+
+	"github.com/gotd/contrib/middleware/floodwait"
 )
 
-// Client abstracts telegram client.
-type Client interface {
+// client abstracts telegram client.
+// implements wrapper for running client in background.
+type client interface {
 	Run(ctx context.Context, f func(ctx context.Context) error) error
 }
 
-// StopFunc closes Client and waits until Run returns.
+// StopFunc closes client and waits until Run returns.
 type StopFunc func() error
 
 type connectOptions struct {
-	ctx context.Context
+	ctx    context.Context
+	waiter *floodwait.Waiter // New field for waiter
 }
 
 // Option for Connect.
@@ -36,12 +39,20 @@ func WithContext(ctx context.Context) Option {
 	})
 }
 
+// WithWaiter sets a waiter for managing flood wait scenarios.
+func WithWaiter(waiter *floodwait.Waiter) Option {
+	return fnOption(func(o *connectOptions) {
+		o.waiter = waiter
+	})
+}
+
 // Connect blocks until client is connected, calling Run internally in
 // background.
-func Connect(client Client, options ...Option) (StopFunc, error) {
+func Connect(client client, options ...Option) (StopFunc, error) {
 	opt := &connectOptions{
 		ctx: context.Background(),
 	}
+
 	for _, o := range options {
 		o.apply(opt)
 	}
@@ -50,23 +61,39 @@ func Connect(client Client, options ...Option) (StopFunc, error) {
 
 	errC := make(chan error, 1)
 	initDone := make(chan struct{})
+
 	go func() {
 		defer close(errC)
-		errC <- client.Run(ctx, func(ctx context.Context) error {
-			close(initDone)
-			<-ctx.Done()
-			if errors.Is(ctx.Err(), context.Canceled) {
-				return nil
-			}
-			return ctx.Err()
-		})
+
+		// Use the waiter to manage retries on FLOOD_WAIT errors.
+		if opt.waiter != nil {
+			errC <- opt.waiter.Run(ctx, func(ctx context.Context) error {
+				return client.Run(ctx, func(ctx context.Context) error {
+					close(initDone)
+					<-ctx.Done()
+					if errors.Is(ctx.Err(), context.Canceled) {
+						return nil
+					}
+					return ctx.Err()
+				})
+			})
+		} else {
+			errC <- client.Run(ctx, func(ctx context.Context) error {
+				close(initDone)
+				<-ctx.Done()
+				if errors.Is(ctx.Err(), context.Canceled) {
+					return nil
+				}
+				return ctx.Err()
+			})
+		}
 	}()
 
 	select {
 	case <-ctx.Done(): // context canceled
 		cancel()
 		return func() error { return nil }, ctx.Err()
-	case err := <-errC: // startup timeout
+	case err := <-errC: // startup error
 		cancel()
 		return func() error { return nil }, err
 	case <-initDone: // init done
